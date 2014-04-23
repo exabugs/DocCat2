c991245562c5bdc15bae2dc366a393c4d5f777da
exabugs <exabugs@gmail.com>
2014-04-24 02:27:01 +0900

    util強化

--- test/text/text_test.js
+++ test/text/text_test.js
@@ -12,89 +12,7 @@ var _ = require('underscore')
 
 describe('text', function () {
 
-  describe('集合演算', function () {
-
-    it('共通集合', function (done) {
-      var array_a = [
-        {k: 70},
-        {k: 10},
-        {k: 50},
-        {k: 30},
-        {k: 90}
-      ];
-      array_a = _.sortBy(array_a, function (value) {
-        return value.k;
-      });
-      var array_b = [
-        {k: 5},
-        {k: 10},
-        {k: 15},
-        {k: 20},
-        {k: 25},
-        {k: 30}
-      ];
-      var result = text.util.intersect(array_a, array_b, 'k');
-      var expected = [
-        [
-          {k: 10},
-          {k: 10}
-        ],
-        [
-          {k: 30},
-          {k: 30}
-        ]
-      ];
-      result.should.eql(expected);
-      done();
-    });
-
-    it('共通集合', function (done) {
-      var array_a = [
-        {k: '70', v: 2},
-        {k: '10', v: 4}, //
-        {k: '50', v: 2},
-        {k: '30', v: 4}, //
-        {k: '90', v: 2}
-      ];
-      array_a = _.sortBy(array_a, function (value) {
-        return value.k;
-      });
-      var array_b = [
-        {k: '10', v: -4}, //
-        {k: '12', v: 2},
-        {k: '15', v: 2},
-        {k: '20', v: 2},
-        {k: '30', v: -4}, //
-        {k: '35', v: 2}
-      ];
-      var result = text.util.intersect(array_a, array_b, 'k');
-      var expected = [
-        [
-          {k: '10', v: 4},
-          {k: '10', v: -4}
-        ],
-        [
-          {k: '30', v: 4},
-          {k: '30', v: -4}
-        ]
-      ];
-      result.should.eql(expected);
-
-      var norm_a = Math.sqrt((4 * 4) + (4 * 4));
-      var norm_b = Math.sqrt((-4 * -4) + (-4 * -4));
-      var norm_x = 4 * (-4) + 4 * (-4);
-      expected = norm_x / norm_a / norm_b;
-
-      var value = text.util.cosine(result, 'v');
-      value.should.eql(expected);
-
-      done();
-    });
-
-
-  });
-
-  describe('parse', function () {
+  describe('parse 1', function () {
     it('should return -1 when the value is not present', function (done) {
       text.parse('食後にコーヒーを飲む', function (err, words) {
         words.should.eql({'食後': 1, 'コーヒー': 1});
@@ -103,7 +21,7 @@ describe('text', function () {
     })
   });
 
-  describe('search', function () {
+  describe('search 1', function () {
     it('should return -1 when the value is not present', function (done) {
       async.waterfall([
         function (next) {
@@ -113,7 +31,7 @@ describe('text', function () {
         },
         function (db, next) {
           var collection = db.collection('mails.files');
-          text.search(collection, {}, 'メールを削除', function (err, result) {
+          text.search(db, collection, null, {}, 'メールを削除', function (err, result) {
             next(err, db);
           })
         }
@@ -124,7 +42,7 @@ describe('text', function () {
     })
   });
 
-  describe('search', function () {
+  describe('search 2', function () {
     it('should return -1 when the value is not present', function (done) {
       async.waterfall([
         function (next) {
@@ -135,6 +53,13 @@ describe('text', function () {
         function (db, next) {
           var collection = db.collection('mails.files');
           text.object_frequency(collection, 'mails.df', function (err, result) {
+            next(err, db);
+          })
+        },
+        function (db, next) {
+          var collection = db.collection('mails.files');
+          var collection_freq = db.collection('mails.df');
+          text.search(db, collection, collection_freq, {}, 'メールを削除', function (err, result) {
             next(err, db);
           })
         }
---
+++ test/text/util_test.js
@@ -0,0 +1,96 @@
+/**
+ * Created by dreamarts on 2014/03/30.
+ */
+
+var _ = require('underscore')
+  , log = require('log')
+  , should = require("should")
+  , util = require('../../lib/text/util')
+  ;
+
+describe('text', function () {
+
+  describe('集合演算', function () {
+
+    it('共通集合', function (done) {
+      var array_a = [
+        {k: 70},
+        {k: 10},
+        {k: 50},
+        {k: 30},
+        {k: 90}
+      ];
+      array_a = _.sortBy(array_a, function (value) {
+        return value.k;
+      });
+      var array_b = [
+        {k: 5},
+        {k: 10},
+        {k: 15},
+        {k: 20},
+        {k: 25},
+        {k: 30}
+      ];
+      var result = util.intersect(array_a, array_b, 'k');
+      var expected = [
+        [
+          {k: 10},
+          {k: 10}
+        ],
+        [
+          {k: 30},
+          {k: 30}
+        ]
+      ];
+      result.should.eql(expected);
+      done();
+    });
+
+    it('共通集合', function (done) {
+      var array_a = [
+        {k: '70', v: 2},
+        {k: '10', v: 4}, //
+        {k: '50', v: 2},
+        {k: '30', v: 4}, //
+        {k: '90', v: 2}
+      ];
+      array_a = _.sortBy(array_a, function (value) {
+        return value.k;
+      });
+      var array_b = [
+        {k: '10', v: -4}, //
+        {k: '12', v: 2},
+        {k: '15', v: 2},
+        {k: '20', v: 2},
+        {k: '30', v: -4}, //
+        {k: '35', v: 2}
+      ];
+      var result = util.intersect(array_a, array_b, 'k');
+      var expected = [
+        [
+          {k: '10', v: 4},
+          {k: '10', v: -4}
+        ],
+        [
+          {k: '30', v: 4},
+          {k: '30', v: -4}
+        ]
+      ];
+      result.should.eql(expected);
+
+      var norm_a = Math.sqrt((4 * 4) + (4 * 4));
+      var norm_b = Math.sqrt((-4 * -4) + (-4 * -4));
+      var norm_x = 4 * (-4) + 4 * (-4);
+      expected = norm_x / norm_a / norm_b;
+
+      var value = util.cosine(result, 'v');
+      value.should.eql(expected);
+
+      done();
+    });
+
+
+  });
+
+
+});
