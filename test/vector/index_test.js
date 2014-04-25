ae0784d5665226785e785e570512a31bea432662
exabugs <exabugs@gmail.com>
2014-04-25 16:21:06 +0900

    比較は関数呼び出しをやめた

--- test/vector/index_test.js
+++ test/vector/index_test.js
@@ -13,38 +13,86 @@ var _ = require('underscore')
 var TEST_COLLECTION = 'test_users';
 
 describe('vector.index', function () {
+
   describe('ライブラリ', function () {
 
-
-    it('cosine', function (done) {
+    it('準備', function (done) {
       var tasks = [];
       tasks.push(function (next) {
-        mongo.open(function(err, db) {
+        mongo.open(function (err, db) {
           next(err, db);
         });
       });
       tasks.push(function (db, next) {
         var collection = db.collection(TEST_COLLECTION);
+        collection.drop(function (err) {
+          next(err, db, collection);
+        });
+      });
+      tasks.push(function (db, collection, next) {
         var data = [
           {
             user_id: 1,
+            feature: [
+              {key: 'a', val: '2'},
+              {key: 'b', val: '2'},
+              {key: 'c', val: '2'}
+            ]
+          },
+          {
+            user_id: 2,
             feature: [
               {key: 'a', val: '1'},
               {key: 'b', val: '1'},
               {key: 'c', val: '1'}
             ]
-          },
-          {
-            user_id: 2
-
           }
         ];
-        next();
+        async.each(data, function (item, next2) {
+          collection.insert(item, function (err) {
+            next2(err);
+          });
+        }, function (err) {
+          next(err, db);
+        })
       });
-      async.waterfall(tasks, function (err, result) {
+      async.waterfall(tasks, function (err, db) {
+        db.close();
         done(err);
       });
     });
+
+    it('cosine', function (done) {
+      var tasks = [];
+      tasks.push(function (next) {
+        mongo.open(function (err, db) {
+          next(err, db);
+        });
+      });
+      tasks.push(function (db, next) {
+        var collection = db.collection(TEST_COLLECTION);
+
+        var condition = {user_id: 1};
+        var target = 'feature';
+        var field = {key: 'key', val: 'val'};
+        var keyword = [
+          {key: 'a', val: 1},
+          {key: 'c', val: 1}
+        ];
+        var weight = {'a': 1, 'b': 1, 'c': 1};
+        var out = {inline: 1};
+
+        //exports.cosine = function (collection, condition, target, field, keyword, weight, out, callback) {
+        vector.cosine(collection, condition, target, field, keyword, weight, out, function (err, result) {
+          next(err, db);
+        });
+      });
+      async.waterfall(tasks, function (err, db) {
+        db.close();
+        done(err);
+      });
+    });
+
   });
 });
 
