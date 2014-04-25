44b502a41c4fac08d0e181c72dae4d917723a602
exabugs <exabugs@gmail.com>
2014-04-25 11:46:13 +0900

    util強化

--- test/text/util_test.js
+++ test/text/util_test.js
@@ -11,6 +11,45 @@ var _ = require('underscore')
   ;
 
 describe('util 2', function () {
+  describe('集合演算', function () {
+    //search: function (array, value, attr, head) {
+    it('共通集合', function () {
+      var array = [
+        {k: 2},
+        {k: 4},
+        {k: 6}
+      ];
+      var result;
+
+      result = text.util.search(array, {k: 0}, 'k');
+      result.should.eql(-2);
+
+      result = text.util.search(array, {k: 1}, 'k');
+      result.should.eql(-2);
+
+      result = text.util.search(array, {k: 2}, 'k');
+      result.should.eql(0);
+
+      result = text.util.search(array, {k: 3}, 'k');
+      result.should.eql(-1);
+
+      result = text.util.search(array, {k: 4}, 'k');
+      result.should.eql(1);
+
+      result = text.util.search(array, {k: 5}, 'k');
+      result.should.eql(-1);
+
+      result = text.util.search(array, {k: 6}, 'k');
+      result.should.eql(2);
+
+      result = text.util.search(array, {k: 7}, 'k');
+      result.should.eql(-3);
+
+      result = text.util.search(array, {k: 8}, 'k');
+      result.should.eql(-3);
+
+    });
+  });
 
   describe('集合演算', function () {
 
@@ -20,7 +59,9 @@ describe('util 2', function () {
         {k: 10},
         {k: 50},
         {k: 30},
-        {k: 90}
+        {k: 90},
+        {k: 91},
+        {k: 92}
       ];
       array_a = _.sortBy(array_a, function (value) {
         return value.k;
@@ -31,8 +72,12 @@ describe('util 2', function () {
         {k: 15},
         {k: 20},
         {k: 25},
-        {k: 30}
+        {k: 30},
+        {k: 35}
       ];
+      array_b = _.sortBy(array_b, function (value) {
+        return value.k;
+      });
       var result = text.util.intersect(array_a, array_b, 'k');
       var expected = [
         [
@@ -70,11 +115,11 @@ describe('util 2', function () {
       var result = text.util.intersect(array_a, array_b, 'k');
       var expected = [
         [
-          {k: '10', v: 4},
+          {k: '10', v: 4}, // 3
           {k: '10', v: -4}
         ],
         [
-          {k: '30', v: 4},
+          {k: '30', v: 4}, // 5
           {k: '30', v: -4}
         ]
       ];
