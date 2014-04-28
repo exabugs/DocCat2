eda0e8cb5a1329e02489aa622ddd74b8e8e2e19a
exabugs <exabugs@gmail.com>
2014-04-28 16:37:24 +0900

    Mail で 形態素解析を採用。

--- test/mail/mail_test.js
+++ test/mail/mail_test.js
@@ -2,34 +2,58 @@
  * Created by dreamarts on 2014/03/30.
  */
 
-var assert = require('assert')
+var should = require('should')
   , log = require('log')
   , fs = require('fs')
-  , MailParser = require("mailparser").MailParser
   , async = require('async')
-
-  , db = require("../../lib/db")
-
+  , mail = require('../../lib/mail')
+  , mongo = require("../../lib/db")
   ;
 
 describe('mail', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function (done) {
+
+  it('DB初期化', function (done) {
+    mongo.open(function (err, db) {
+      var colls = ['mails', 'mails.files', 'mails.chunks', 'mails.df'];
+      async.each(colls, function (coll, next) {
+        db.collection(coll).remove({}, function (err, reply) {
+          next();
+        });
+      }, function (err) {
+        db.close();
+        done();
+      });
+    });
+  });
+
+  it('メールをDBに追加する', function (done) {
+    var path = 'test/mail/test.eml';
+    mail.add(path, function (err, mailInfo) {
+      mail.get({"messageId": "1397023383.498516.1.1000481@moe.dreamarts.co.jp"}, function (err, result) {
+        should.not.exist(err);
+        result.length.should.eql(1);
+        done();
+      });
+    });
+  });
+
+  /**
+   it('should return -1 when the value is not present', function (done) {
 
       var array = [];
       async.waterfall([
-        function(next) {
+        function (next) {
           for (var i = 0; i < 3; i++) {
-             var item = {stream: fs.createReadStream('test/mail/test.eml')};
+            var item = {stream: fs.createReadStream('test/mail/test.eml')};
             array.push(item);
           }
           next(null, item);
         },
-        function(item, next) {
-          async.eachSeries(array, function(tgt, next1) {
+        function (item, next) {
+          async.eachSeries(array, function (tgt, next1) {
             var ws = fs.createWriteStream('/tmp/test.eml');
             item.stream.pipe(ws);
-            ws.on('close', function() {
+            ws.on('close', function () {
               next1();
             });
 
@@ -42,7 +66,7 @@ describe('mail', function () {
         done();
       });
     });
-  })
-})
+   */
+});
 
 
