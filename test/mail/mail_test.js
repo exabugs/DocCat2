ce008615e7e7327265d66a026ce972969337d672
exabugs <exabugs@gmail.com>
2014-04-29 02:05:06 +0900

    メール テストデータ整理

--- test/mail/mail_test.js
+++ test/mail/mail_test.js
@@ -7,12 +7,13 @@ var should = require('should')
   , fs = require('fs')
   , async = require('async')
   , mail = require('../../lib/mail')
+  , frequency = require('../../lib/text/frequency')
   , mongo = require("../../lib/db")
   ;
 
 describe('mail', function () {
 
-  it('DB初期化', function (done) {
+  function init_db(done) {
     mongo.open(function (err, db) {
       var colls = ['mails', 'mails.files', 'mails.chunks', 'mails.df'];
       async.each(colls, function (coll, next) {
@@ -20,53 +21,72 @@ describe('mail', function () {
           next();
         });
       }, function (err) {
-        db.close();
-        done();
+        done(err, db);
+      });
+    });
+  }
+
+  it('追加 1', function (done) {
+    init_db(function (err, db) {
+      var path = 'test/mail/data/test_1.eml';
+      mail.add(path, function (err, mailInfo) {
+        mail.get({"messageId": "1397023383.498516.1.1000481@moe.dreamarts.co.jp"}, function (err, result) {
+          should.not.exist(err);
+          result.length.should.eql(1);
+          db.close();
+          done();
+        });
       });
     });
   });
 
-  it('メールをDBに追加する', function (done) {
-    var path = 'test/mail/data/test_1.eml';
-    mail.add(path, function (err, mailInfo) {
-      mail.get({"messageId": "1397023383.498516.1.1000481@moe.dreamarts.co.jp"}, function (err, result) {
-        should.not.exist(err);
-        result.length.should.eql(1);
-        done();
+  // 上記の「追加」がテストOKなので、ここではライブラリ的に使います。
+  function add(array, callback) {
+    async.each(array, function(item, next) {
+      mail.add(item, function (err) {
+        next(err);
+      });
+    }, function(err) {
+      callback(err);
+    });
+  }
+
+  it('追加 2', function (done) {
+    init_db(function (err, db) {
+      var data = [
+        'test/mail/data/test_1.eml', // "1397023383.498516.1.1000481@moe.dreamarts.co.jp"
+        'test/mail/data/test_2.eml'  // "EBEBFB71-96DD-4FF0-9787-49B4D8A684E4@dreamarts.co.jp"
+        ];
+      add(data, function (err) {
+        mail.get({"messageId": "1397023383.498516.1.1000481@moe.dreamarts.co.jp"}, function (err, result) {
+          should.not.exist(err);
+          result.length.should.eql(1);
+          done();
+        });
       });
     });
   });
 
-  /**
-   it('should return -1 when the value is not present', function (done) {
-
-      var array = [];
-      async.waterfall([
-        function (next) {
-          for (var i = 0; i < 3; i++) {
-            var item = {stream: fs.createReadStream('test/mail/test.eml')};
-            array.push(item);
-          }
-          next(null, item);
-        },
-        function (item, next) {
-          async.eachSeries(array, function (tgt, next1) {
-            var ws = fs.createWriteStream('/tmp/test.eml');
-            item.stream.pipe(ws);
-            ws.on('close', function () {
-              next1();
-            });
-
-          }, function (err) {
-            next(null);
-
-          });
-        }
-      ], function (err) {
-        done();
+  it('追加 2', function (done) {
+    init_db(function (err, db) {
+      var data = [
+        'test/mail/data/test_2.eml'  // "EBEBFB71-96DD-4FF0-9787-49B4D8A684E4@dreamarts.co.jp"
+      ];
+      add(data, function (err) {
+        var collection = db.collection('mails.files');
+        var attribute = 'metadata.tf'
+        var field = {k: 'k', v: 'c'};
+        var option = {
+          condition: {"metadata.origin.messageId": "EBEBFB71-96DD-4FF0-9787-49B4D8A684E4@dreamarts.co.jp"}
+        };
+        frequency.countup_one(collection, attribute, field, option, function (err, result) {
+          should.not.exist(err);
+          done();
+        });
       });
     });
-   */
+  });
+
 });
 
 
