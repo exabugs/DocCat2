21079af03bd0f2dbc2a6204bf0c15d2e2777fddf
exabugs <exabugs@gmail.com>
2014-05-20 11:30:08 +0900

    Refactor.

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -58,6 +58,85 @@ function plot(json) {
 
 }
 
+function svg(json) {
+
+  var min_x = 0;
+  var max_x = 0;
+  var min_y = 0;
+  var max_y = 0;
+  var min_s = 0;
+  var max_s = 0;
+
+  _.each(json.items, function (item) {
+    var info = item.tf;
+    if (info) {
+      if (info.x < min_x) min_x = info.x;
+      if (info.x > max_x) max_x = info.x;
+      if (info.y < min_y) min_y = info.y;
+      if (info.y > max_y) max_y = info.y;
+      if (info.score < min_s) min_s = info.score;
+      if (info.score > max_s) max_s = info.score;
+    }
+  });
+
+  var d_x = 0.3;
+  var d_y = 0.1;
+
+  min_x -= d_x;
+  max_x += d_x;
+  min_y -= d_y;
+  max_y += d_y;
+
+  //var svg = $('#svg_id');
+  var svg = document.getElementById('svg_id');
+
+  var w = max_x - min_x;
+  var h = max_y - min_y;
+  svg.setAttribute('viewBox', [min_x, min_y, w, h].join(' '));
+
+//  var text = $('<text x="20" y="20">ごきげんよう。</text>');
+//  svg.append(text);
+
+
+  var s = 40; // 直径
+
+//  var view_w = w / (max_x - min_x);
+//  var view_h = h / (max_y - min_y);
+  var view_s = s / (max_s - min_s);
+
+  //svg.empty();
+
+  _.each(json.items, function (item) {
+    var info = item.tf;
+    if (info) {
+      var x = info.x;
+      var y = info.y;
+/*
+      ctx.fillText(item.subject, x, y);
+
+      ctx.beginPath();
+      ctx.arc(x, y, info.score * view_s + 2, 0, PI2, false);
+      ctx.stroke();
+
+      <circle cx="100" cy="50" r="40" stroke="black" stroke-width="2" fill="red" />
+*/
+      //var circle = $('<circle>');
+      var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      circle.setAttribute('cx', x);
+      circle.setAttribute('cy', y);
+      circle.setAttribute('r', info.score * 10);
+      circle.setAttribute('stroke', 'black');
+      circle.setAttribute('stroke-width', 1);
+      circle.setAttribute('fill', 'none');
+      circle.setAttribute('vector-effect', 'non-scaling-stroke');
+
+      svg.appendChild(circle);
+    }
+
+  });
+
+}
+
 $(document).ready(function () {
   var myTable = $('#table_id').dataTable({
     bAutoWidth: false,
@@ -101,7 +180,8 @@ $(document).ready(function () {
           return json;
         })
         .done(fnCallback)
-        .done(plot);
+        .done(plot)
+        .done(svg);
 
     }
   });
